dfbf2318697175efc2d918fff4b0194098c48664
Michael Moroney <mmoroney06@gmail.com>
2018-12-17 12:29:00 -0600

    add books controler, show route, migration

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,6 @@
+class BooksController < ApplicationController
+  def show
+  	#display one book and its attributes
+  	render "show"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root "index#index"
+
+  resources :books, only: [:show]
 end
