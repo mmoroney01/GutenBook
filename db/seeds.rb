dba7f85b2511e3308fe003b928d12f6f6c540437
Michael Moroney <mmoroney06@gmail.com>
2019-01-03 16:00:04 -0600

    sample text now begins in the correct place

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,9 @@ def book_attributes(txt)
     searchable_text = []
 
     #params of open may need to be different depending on source of txt files
-    File.open(txt).each do |line|
+    File.open(txt).each.each_with_index do |line, index|
       if line.match(/^Title: /)
-        title = line.gsub(/Title: /, "").gsub(/\r\n/,'')
+        title = line.gsub(/Title: /, "").gsub(/\r\n/,'').titleize
       end
 
       if line.match(/^Author: /)
@@ -27,7 +27,7 @@ def book_attributes(txt)
       end
 
       line.split.each do |word|
-        #gets a string containing first 1000 words
+        #gets a string containing first 1000-ish words
         if sample_text.split(" ").length < 1000
           sample_text+=word+=" "
         end
@@ -39,15 +39,24 @@ def book_attributes(txt)
         #gets all text, unaltered
         presentable_text << word
 
-        #gets all text minus non-word/number characters, useful for searching. May need to downcase all words as searchkick's default is to search for lowercase word?
+        #gets all text minus non-word/number characters, useful for searching
         searchable_text << word.gsub(/[^a-zA-Z\d]/, '')
       end
+    end
+    
+    #this ensures all sample text starts in the right place
+    if sample_text.include?(title)
+      sample_text = title + sample_text.split(title)[1]
+    elsif sample_text.include?(title.upcase)
+      sample_text = title.upcase + sample_text.split(title.upcase)[1]
+    elsif sample_text.include?(title.downcase)
+      sample_text = title.downcase + sample_text.split(title.downcase)[1]
     end
 
     return {title: title, author: author, release_date: release_date, language: language, presentable_text: presentable_text, searchable_text: searchable_text, sample_text: sample_text}
 end
 
-txt = {"TheCaskOfAmontillado.txt" => "NoCover.jpg", "TheFallOfTheHouseOfUsher.txt" => "NoCover.jpg", "TheRaven.txt" => "TheRaven.jpg", "TheMasqueOfTheRedDeath.txt" => "NoCover.jpg", "EurekaAProsePoem.txt" => "NoCover.jpg"}
+txt = {"EurekaAProsePoem.txt" => "NoCover.jpg", "TheFallOfTheHouseOfUsher.txt" => "NoCover.jpg", "TheRaven.txt" => "TheRaven.jpg", "TheMasqueOfTheRedDeath.txt" => "NoCover.jpg", "TheCaskOfAmontillado.txt" => "NoCover.jpg"}
 
 txt.each do |txt, img|
   book_hash = book_attributes(txt)
